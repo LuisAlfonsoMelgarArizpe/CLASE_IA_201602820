530bd56a204cbd931ef2512022eddfb895155e41
LuisAlfonsoMelgarArizpe <alfonso.melgar97@gmail.com>
2021-06-21 16:56:12 -0600

    api xd

--- Proyecto1/index.js
+++ Proyecto1/index.js
@@ -1,4 +1,8 @@
 console.log("test")
 document.open();
-document.write("1");
+var url = new URL(window.location.href);
+var turno = url.searchParams.get("turno");
+var estado = url.searchParams.get("estado");
+
+document.write("turno : " + turno + "<br> estado : " + estado);
 document.close();
